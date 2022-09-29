e36eea54cd34e1b9d7844c625c84fc28bb9a13e5
Jonathan Lehr <jonathan@aboutobjects.com>
2022-09-29 16:41:38 -0400

    Added controls

--- Coolness/Coolness/CLNCoolViewController.m
+++ Coolness/Coolness/CLNCoolViewController.m
@@ -4,16 +4,64 @@
 #import "CLNCoolViewController.h"
 #import "CLNCoolViewCell.h"
 
+@interface CLNCoolViewController ()
+@property (strong, nonatomic) UITextField *textField;
+@property (strong, nonatomic) UIView *contentView;
+@end
+
 @implementation CLNCoolViewController
+
+- (void)addCell {
+    NSLog(@"In %s", __func__);
+    CLNCoolViewCell *newCell = [[CLNCoolViewCell alloc] init];
+    newCell.text = self.textField.text;
+    newCell.backgroundColor = UIColor.systemGreenColor;
+    [self.contentView addSubview:newCell];
+}
 
 - (void)loadView {
     self.view = [[UIView alloc] init];
     self.view.backgroundColor = UIColor.systemBrownColor;
     
+    CGRect screenRect = UIScreen.mainScreen.bounds;
+    CGRect accessoryRect;
+    CGRect contentRect;
+    CGRectDivide(screenRect, &accessoryRect, &contentRect, 105, CGRectMinYEdge);
+    
+    UIView *accessoryView = [[UIView alloc] initWithFrame:accessoryRect];
+    self.contentView = [[UIView alloc] initWithFrame:contentRect];
+    
+    self.contentView.clipsToBounds = YES;
+    
+    [self.view addSubview:accessoryView];
+    [self.view addSubview:self.contentView];
+    
+    accessoryView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.6];
+    self.contentView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.4];
+    
+    // Controls
+    
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(20, 60, 240, 36)];
+    [accessoryView addSubview:self.textField];
+    self.textField.borderStyle = UITextBorderStyleRoundedRect;
+    self.textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    self.textField.placeholder = @"Type something here";
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [accessoryView addSubview:button];
+    [button setTitle:@" Add Cell " forState:UIControlStateNormal];
+    [button setTitle:@"Adding..." forState:UIControlStateHighlighted];
+    [button sizeToFit];
+    button.frame = CGRectOffset(button.frame, 280, 60);
+    
+    [button addTarget:self action:@selector(addCell) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Cells
+    
     CLNCoolViewCell *subview1 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(20, 60, 240, 40)];
     CLNCoolViewCell *subview2 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(40, 120, 240, 40)];
-    [self.view addSubview:subview1];
-    [self.view addSubview:subview2];
+    [self.contentView addSubview:subview1];
+    [self.contentView addSubview:subview2];
     
     subview1.text = @"Hello World! 🌎🌏🌍";
     subview2.text = @"Cool View Cells Rock! 🥂🍾";
